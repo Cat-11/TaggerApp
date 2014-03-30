50355371cdca4efac93e4af4069156eac7d5d944
Andrew Khoury <andrewkhoury@gmail.com>
2014-03-30 10:17:28 -0700

    Added timestamp to all messages.

--- tagger-main/src/main/java/com/cat11/tagger/main/Message.java
+++ tagger-main/src/main/java/com/cat11/tagger/main/Message.java
@@ -1,8 +1,19 @@
 package com.cat11.tagger.main;
 
+import java.util.Date;
+
 public class Message {
 
-    private String content;
+    private String     content;
+    private final Date time;
+
+    public Date getTime() {
+        return this.time;
+    }
+
+    public Message() {
+        this.time = new Date();
+    }
 
     public String getContent() {
         return this.content;
