660a263e6bd49bd733f84afba1851841f4da3e8f
Andrew Khoury <andrewkhoury@gmail.com>
2014-03-29 23:21:11 -0700

    Created basic messages functionality

---
+++ tagger-main/src/main/java/com/cat11/tagger/main/Message.java
@@ -0,0 +1,14 @@
+package com.cat11.tagger.main;
+
+public class Message {
+
+    private String content;
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
+}
--- tagger-main/src/main/java/com/cat11/tagger/main/Tagger.java
+++ tagger-main/src/main/java/com/cat11/tagger/main/Tagger.java
@@ -1,15 +1,37 @@
 package com.cat11.tagger.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Tagger {
+
+    private static final List<Message> messages = new ArrayList<Message>();
 
     @RequestMapping("/status")
     @ResponseBody
     public String status() {
         return "Tagger server is up and running.";
     }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Message> messages() {
+        return messages;
+    }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseBody
+    public
+            Message publishMessage(@RequestBody final Message message) {
+        messages.add(message);
+        return message;
+    }
+
 }
